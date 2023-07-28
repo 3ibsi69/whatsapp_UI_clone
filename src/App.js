2a48a52405939b4b808b8eb4ba10e54631bad3e0
3ibsi69 <a.abbassi21306@pi.tn>
2023-07-29 00:39:31 +0100

    side navs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
 import './Styles/MainStyle.css';
+import SideBar from './Components/SidebarComponnets/Sidebar';
+
 function App() {
   return (
-    <div className="greencontainer">
+    <div>
+      <div className="green-container">
 
+      </div>
+      <div className="main-container">
+      <SideBar/>
+      </div>
     </div>
   );
 }
---
+++ src/Components/SidebarComponnets/Sidebar.jsx
@@ -0,0 +1,14 @@
+import "../../Styles/SideBar.css";
+import Searchnav from "./Searchnav";
+import SideNav from "./SideNav";
+function SideBar() {
+  return (
+    <>
+    <SideNav/>
+    
+    <Searchnav/>
+    </>
+    );
+}
+
+export default SideBar;
