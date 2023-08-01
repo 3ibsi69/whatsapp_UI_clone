d9157dff7c9d8f6049df731b1b526b268009bc5a
3ibsi69 <a.abbassi21306@pi.tn>
2023-08-01 20:35:14 +0100

    fixed image

--- src/Components/SidebarComponnets/Sidebar.jsx
+++ src/Components/SidebarComponnets/Sidebar.jsx
@@ -2,6 +2,12 @@ import "../../Styles/SideBar.css";
 import Chats from "./Chats";
 import Searchnav from "./Searchnav";
 import SideNav from "./SideNav";
+import melek from "../../Assets/melek.jpg";
+import crush from "../../Assets/crush.jfif";
+import belha from "../../Assets/belha.jpg";
+import anas from "../../Assets/anas.jpg";
+import men from "../../Assets/men.avif";
+
 function SideBar() {
   return (
     <>
@@ -12,35 +18,35 @@ function SideBar() {
         chatTime="2:00 PM"
         messageCounter={1}
         chatText="Hey, how are you?"
-        chatImgSrc="https://www.w3schools.com/howto/img_avatar.png"
+        chatImgSrc={`${men}`}
       />
        <Chats
         chatName="Anas Abbassi"
         chatTime="4:30 PM"
         messageCounter={98}
         chatText="Come on, let's go to the cinema"
-        chatImgSrc="https://www.w3schools.com/howto/img_avatar.png"
+        chatImgSrc={`${anas}`}
       />
        <Chats
         chatName="Melek Abbassi"
         chatTime="2:00 AM"
         messageCounter={5}
         chatText="Good night"
-        chatImgSrc="https://www.w3schools.com/howto/img_avatar.png"
+        chatImgSrc={`${melek}`}
       />
        <Chats
         chatName="Crush"
         chatTime="12:26 PM"
         messageCounter={0}
         chatText="I love you"
-        chatImgSrc="https://www.w3schools.com/howto/img_avatar.png"
+        chatImgSrc={`${crush}`}
       />
        <Chats
         chatName="Dad"
         chatTime="9:45 PM"
         messageCounter={95}
         chatText="Come back home now if you don't come i will beat you up so hard" 
-        chatImgSrc="https://www.w3schools.com/howto/img_avatar.png"
+        chatImgSrc={`${belha}`}
       />
       
     </>
