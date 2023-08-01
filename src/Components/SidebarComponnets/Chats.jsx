9e33d5be0bc04e7de573b503d128a3d3febb83a9
3ibsi69 <a.abbassi21306@pi.tn>
2023-08-01 19:03:28 +0100

    chats added

---
+++ src/Components/SidebarComponnets/Chats.jsx
@@ -0,0 +1,20 @@
+import "../../Styles/Chats.css"
+function Chats(){
+    return(
+        <div className="chat-container">
+            <div className="chat">
+                <div className="chat-img">
+                    </div>
+                    <div className="chat-content">
+                        <div className="chat-header">
+                            <div className="chat-name">John Doe</div>
+                            </div>
+                            <div className="chat-text">Hey, how are you?</div>
+                            </div>
+                            </div>
+        </div>
+    )
+}
+
+
+export default Chats;
--- src/Components/SidebarComponnets/Sidebar.jsx
+++ src/Components/SidebarComponnets/Sidebar.jsx
@@ -1,12 +1,13 @@
 import "../../Styles/SideBar.css";
+import Chats from "./Chats";
 import Searchnav from "./Searchnav";
 import SideNav from "./SideNav";
 function SideBar() {
   return (
     <>
     <SideNav/>
-    
     <Searchnav/>
+    <Chats/>
     </>
     );
 }
