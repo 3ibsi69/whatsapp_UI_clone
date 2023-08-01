5ab7fe519c2a41436ac03736cac68c9584d428a0
3ibsi69 <a.abbassi21306@pi.tn>
2023-08-01 20:09:56 +0100

    fixed all the chats

--- src/Components/SidebarComponnets/Sidebar.jsx
+++ src/Components/SidebarComponnets/Sidebar.jsx
@@ -7,7 +7,42 @@ function SideBar() {
     <>
     <SideNav/>
     <Searchnav/>
-    <Chats/>
+    <Chats
+        chatName="John Doe"
+        chatTime="2:00 PM"
+        messageCounter={1}
+        chatText="Hey, how are you?"
+        chatImgSrc="https://www.w3schools.com/howto/img_avatar.png"
+      />
+       <Chats
+        chatName="Anas Abbassi"
+        chatTime="4:30 PM"
+        messageCounter={98}
+        chatText="Come on, let's go to the cinema"
+        chatImgSrc="https://www.w3schools.com/howto/img_avatar.png"
+      />
+       <Chats
+        chatName="Melek Abbassi"
+        chatTime="2:00 AM"
+        messageCounter={5}
+        chatText="Good night"
+        chatImgSrc="https://www.w3schools.com/howto/img_avatar.png"
+      />
+       <Chats
+        chatName="Crush"
+        chatTime="12:26 PM"
+        messageCounter={0}
+        chatText="I love you"
+        chatImgSrc="https://www.w3schools.com/howto/img_avatar.png"
+      />
+       <Chats
+        chatName="Dad"
+        chatTime="9:45 PM"
+        messageCounter={95}
+        chatText="Come back home now if you don't come i will beat you up so hard" 
+        chatImgSrc="https://www.w3schools.com/howto/img_avatar.png"
+      />
+      
     </>
     );
 }
